8d5ab3bcddb1b3757531e6f5e35d9df2d6bc5eca
Dean <deancs1997@gmail.com>
2019-10-18 19:07:25 -0400

    sw2.1: finished layout
    sw2.1:  finished building the layout for main screen

--- App/MediaBender/app/src/main/java/com/example/mediabender/MainActivity.kt
+++ App/MediaBender/app/src/main/java/com/example/mediabender/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mediabender
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
 }
