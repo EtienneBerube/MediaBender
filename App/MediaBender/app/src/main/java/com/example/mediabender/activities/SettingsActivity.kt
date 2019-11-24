5daeb8e92316f55cc6d77e419c14052a0a88c913
Dean <deancs1997@gmail.com>
2019-11-24 15:36:02 -0500

    Added the deleted code for the stepper

--- App/MediaBender/app/src/main/java/com/example/mediabender/activities/SettingsActivity.kt
+++ App/MediaBender/app/src/main/java/com/example/mediabender/activities/SettingsActivity.kt
@@ -23,8 +23,8 @@ import com.example.mediabender.service.Sensibility
 import com.example.mediabender.service.SerialCommunicationService
 import com.example.mediabender.service.ServiceRequest
 import kotlinx.android.synthetic.main.activity_settings.*
-//import nl.dionsegijn.steppertouch.OnStepCallback
-//import nl.dionsegijn.steppertouch.StepperTouch
+import nl.dionsegijn.steppertouch.OnStepCallback
+import nl.dionsegijn.steppertouch.StepperTouch
 
 
 class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionDialogListener {
@@ -37,6 +37,7 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
     private lateinit var settingsActivity: View
     private lateinit var nightModeButton: NightModeButton
     private lateinit var playerSharedPreferenceHelper: PlayerAccountSharedPreferenceHelper
+    private lateinit var stepperTouch: StepperTouch
     private var darkThemeChosen = true
     private lateinit var installedPlayers: List<ApplicationInfo>
 
@@ -50,10 +51,10 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
         supportActionBar?.elevation = 0f
 
         playerSharedPreferenceHelper = PlayerAccountSharedPreferenceHelper(
-                getSharedPreferences(
-                        "Player Accounts",
-                        Context.MODE_PRIVATE
-                )
+            getSharedPreferences(
+                "Player Accounts",
+                Context.MODE_PRIVATE
+            )
         )
         getThemeSaved()
         setupSettings()
@@ -110,9 +111,9 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
 
         spotifyViewHolder.connectedTextView = findViewById(R.id.spotify_connection_textview)
         spotifyViewHolder.setConnectedTextWithEmail(
-                playerSharedPreferenceHelper.getPlayerAccount(
-                        MediaPlayer.SPOTIFY
-                )
+            playerSharedPreferenceHelper.getPlayerAccount(
+                MediaPlayer.SPOTIFY
+            )
         )
 
         if (MediaPlayer.SPOTIFY.packageName !in installedPlayers.map { player -> player.packageName }) {
@@ -131,9 +132,9 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
 
         googlePlayViewHolder.connectedTextView = findViewById(R.id.google_play_connection_textview)
         googlePlayViewHolder.setConnectedTextWithEmail(
-                playerSharedPreferenceHelper.getPlayerAccount(
-                        MediaPlayer.GOOGLE_PLAY
-                )
+            playerSharedPreferenceHelper.getPlayerAccount(
+                MediaPlayer.GOOGLE_PLAY
+            )
         )
 
         if (MediaPlayer.GOOGLE_PLAY.packageName !in installedPlayers.map { player -> player.packageName }) {
@@ -152,9 +153,9 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
 
         appleMusicViewHolder.connectedTextView = findViewById(R.id.apple_music_connection_textview)
         appleMusicViewHolder.setConnectedTextWithEmail(
-                playerSharedPreferenceHelper.getPlayerAccount(
-                        MediaPlayer.APPLE_MUSIC
-                )
+            playerSharedPreferenceHelper.getPlayerAccount(
+                MediaPlayer.APPLE_MUSIC
+            )
         )
 
         if (MediaPlayer.APPLE_MUSIC.packageName !in installedPlayers.map { player -> player.packageName }) {
@@ -166,7 +167,7 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
         settingsLabel = findViewById(R.id.settingsTitle)
         settingsActivity = findViewById(R.id.settings_parent_scroll)
         accountsLabel = findViewById(R.id.accountsTitle)
-
+        stepperTouch = findViewById(R.id.StepperTouch)
         nightModeButton = findViewById(R.id.nightModeButton)
         nightModeButton.setModeFirstTime(darkThemeChosen)
 
@@ -174,15 +175,16 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
         findViewById<Button>(R.id.settings_gesture_button).setOnClickListener { remapGesture() }
 
         nightMode()
+        setUpStepper()
 
     }
 
     private fun testSensorConnection() {
         //TODO implement later
         val toast = Toast.makeText(
-                applicationContext,
-                "Testing connection not implemented yet",
-                Toast.LENGTH_SHORT
+            applicationContext,
+            "Testing connection not implemented yet",
+            Toast.LENGTH_SHORT
         )
         toast.show()
     }
@@ -195,7 +197,7 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
 
     fun setRunningIndicator(playerPackage: MediaPlayer, active: Boolean) {
         val resource =
-                if (active) R.drawable.active_circle_account else R.drawable.inactive_circle_account
+            if (active) R.drawable.active_circle_account else R.drawable.inactive_circle_account
 
         when (playerPackage) {
             MediaPlayer.SPOTIFY -> {
@@ -249,7 +251,7 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
 
     private fun loadAppropriateTheme() {
         val currentMode =
-                settingsActivity.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            settingsActivity.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
 
 
         if (currentMode == Configuration.UI_MODE_NIGHT_YES || darkThemeChosen) loadDarkTheme()
@@ -259,7 +261,7 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
     // To make the drop down menu for Themes to show the right saved theme
     private fun getThemeSaved() {
         val themeHelper =
-                ThemeSharedPreferenceHelper(getSharedPreferences("Theme", Context.MODE_PRIVATE))
+            ThemeSharedPreferenceHelper(getSharedPreferences("Theme", Context.MODE_PRIVATE))
         val savedTheme = themeHelper.getTheme()
 
         when (savedTheme) {
@@ -291,11 +293,11 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
 
                 if (settingsActivity.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) {
 
-                        Toast.makeText(
-                            getApplicationContext(),
-                            "Dark Theme will remain because Low Power Mode is Enabled to save energy",
-                            Toast.LENGTH_LONG
-                        ).show();
+                    Toast.makeText(
+                        getApplicationContext(),
+                        "Dark Theme will remain because Low Power Mode is Enabled to save energy",
+                        Toast.LENGTH_LONG
+                    ).show();
 
                 }
                 saveTheme(darkThemeChosen)
@@ -321,6 +323,87 @@ class SettingsActivity : AppCompatActivity(), PlayerConnectionDialog.ConnectionD
                 themeSharedPreferenceHelper.saveTheme("Light")
             }
         }
+    }
+
+    private fun setUpStepper() {
+        //define minimum value of sensitivity stepper to 0
+        stepperTouch.minValue = 1
+        //allow side taps
+        stepperTouch.sideTapEnabled = true
+        //set maximum value of sensitivity stepper to 4
+        stepperTouch.maxValue = 3
+
+        //add callback for the sensitivity stepper
+        stepperTouch.addStepCallback(
+            object : OnStepCallback {
+                override fun onStep(value: Int, positive: Boolean) {
+                    //switch statement to initiate action when the stepper changes the value
+                    //add code to each case to tell arduino to switch sensitivity
+                    when (value) {
+                        1 -> {
+                            if (SerialCommunicationService.instance.isConnected) {
+                                SerialCommunicationService.instance.sendRequest(
+                                    ServiceRequest(Request.SENSIBILITY, Sensibility.LOW)
+                                )
+                            } else {
+                                Toast.makeText(applicationContext, "Connect the sensor first", Toast.LENGTH_SHORT).show()
+                            }
+                            Toast.makeText(
+                                applicationContext,
+                                "Low sensitivity",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        2 -> {
+                            if (SerialCommunicationService.instance.isConnected) {
+                                SerialCommunicationService.instance.sendRequest(
+                                    ServiceRequest(
+                                        Request.SENSIBILITY,
+                                        Sensibility.MEDIUM
+                                    )
+                                )
+                            } else {
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Connect the sensor first",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            Toast.makeText(
+                                applicationContext,
+                                "Medium sensitivity",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        3 -> {
+                            if (SerialCommunicationService.instance.isConnected) {
+                                SerialCommunicationService.instance.sendRequest(
+                                    ServiceRequest(
+                                        Request.SENSIBILITY,
+                                        Sensibility.HIGH
+                                    )
+                                )
+                            } else {
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Connect the sensor first",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            Toast.makeText(
+                                applicationContext,
+                                "High sensitivity",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        else -> {
+                            // Do nothing
+                        }
+                    }
+                }
+            }
+        )
+
     }
 }
 
