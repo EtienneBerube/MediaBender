d007135bcbbfc5806417161a7155468e78845b30
Victor Tudoran <tudoranv@outlook.com>
2019-11-24 11:46:30 -0500

    Answer call

--- App/MediaBender/app/src/main/java/com/example/mediabender/PhoneControls.kt
+++ App/MediaBender/app/src/main/java/com/example/mediabender/PhoneControls.kt
@@ -1,29 +1,54 @@
 package com.example.mediabender
 
 import android.content.Context
+import android.telecom.Call
 import android.content.Intent
+import android.telecom.InCallService
+import android.telecom.TelecomManager
 import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
 import android.widget.Toast
 
 class PhoneControls(val context: Context) {
 
-    private var telephony: TelephonyManager
+    var telephony: TelephonyManager
+    lateinit var telecomManager : TelecomManager
+    lateinit var call : Call
 
     init {
-        // google play android actions
-        //iF.addAction("TelephonyManager.ACTION_PHONE_STATE_CHANGED")
 
         var phoneListener = MyPhoneListener(context)
         telephony = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-
         telephony.listen(phoneListener, PhoneStateListener.LISTEN_CALL_STATE)
+
+        telecomManager = context.getSystemService(Context.TELECOM_SERVICE) as TelecomManager
+
+
+    }
+
+   fun answerCall() {
+
+
+    }
+
+
+    fun declineCall() {
+
+        this.call.reject(true, "Cannot Talk. Currently Driving")
+
+    }
+
+
+    fun disconnectCall() {
+
+        this.call.disconnect()
 
     }
 
 
     inner class MyPhoneListener(val context: Context) : PhoneStateListener() {
-        var phoneRinging = false
+        private var phoneRinging = false
+
 
         override fun onCallStateChanged(state: Int, incomingNumber : String){
 
@@ -32,12 +57,16 @@ class PhoneControls(val context: Context) {
                 TelephonyManager.CALL_STATE_OFFHOOK -> {
                     phoneRinging = false
                     Toast.makeText(context, "Call Active", Toast.LENGTH_SHORT).show()
+
                 }
                 TelephonyManager.CALL_STATE_RINGING -> {
                     phoneRinging = true
                     Toast.makeText(context, "Call Listener", Toast.LENGTH_SHORT).show()
+                    answerCall()
+
                 }
             }
+
 
         }
 
